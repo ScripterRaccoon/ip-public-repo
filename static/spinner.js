e7c3595d6bc9c6ae583b795c2e5b42c2abe274d6
Nicolas Nuñez <nicolas999nunez999@gmail.com>
2024-06-30 14:42:37 -0300

    Mejor Spinner

--- static/spinner.js
+++ static/spinner.js
@@ -1,17 +1,4 @@
-window.addEventListener("DOMContentLoaded", () => {
-    showLoader();
-})
-window.addEventListener("load", () => {
-    setTimeout(() => {
-        hideLoader();
-    }, 1000)
-})
-
-const showLoader = () => {
-    const loader = document.getElementById("loader_pagina")
-    loader.classList.add("show_loader")
-}
-const hideLoader = () => {
-    const loader = document.getElementById("loader_pagina")
-    loader.classList.remove("show_loader")
+window.onload = function(){
+    $("#onload").fadeOut();
+    $("body").removeClass("hidden");
 }
